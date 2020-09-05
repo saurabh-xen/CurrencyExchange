c88a841659385dcbd446fc507707522a3d5b89bf
Saurabh Chaurasia <h4saurabh@gmail.com>
2020-09-05 14:06:49 +0530

    first commit

---
+++ CurrencyExchange.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('form').onsubmit = function() {
+
+        // Send a GET request 
+        fetch('https://api.exchangeratesapi.io/latest?base=INR')
+        // Put response into json form
+        
+        .then(response => response.json())
+        .then(data => {
+            // Get currency from user input
+            const currency = document.querySelector('#currency').value.toUpperCase();
+
+            // Get rate from data
+            const rate = data.rates[currency];
+
+            // Check if currency is valid:
+            if (rate !== undefined) {
+                
+                document.querySelector('#result').innerHTML = `1 INR is equal to ${rate.toFixed(3)} ${currency}.`;
+            }
+            else {
+                
+                document.querySelector('#result').innerHTML = 'Invalid Currency.';
+            }
+        })
+        // Catch any errors and log them to the console
+        .catch(error => {
+            console.log('Error:', error);
+        });
+        // Prevent default submission
+        return false;
+    }
+});
